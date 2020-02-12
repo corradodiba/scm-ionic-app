3c0ec1e9734d6701d78592f3ef982003e2ab27ca
tanocarfi <tanocarfi@gmail.com>
2020-02-12 17:40:17 +0100

    added fab-button component

---
+++ src/app/models/FabIcon.model.ts
@@ -0,0 +1,5 @@
+export default interface FabIcon {
+  name?: string;
+  icon: string;
+  action?: any;
+}
--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -9,9 +9,16 @@ import { UsersRoutingModule } from './users-routing.module';
 import { UserListPage } from './user-list/user-list.page';
 import { UserDetailPage } from './user-detail/user-detail.page';
 import { MenuButtonModule } from 'src/app/components/menu-button/menu-button.module';
+import { FabButtonComponent } from 'src/app/components/fab-button/fab-button.component';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, IonicModule, UsersRoutingModule, MenuButtonModule],
-    declarations: [UserListPage, UserDetailPage],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    UsersRoutingModule,
+    MenuButtonModule,
+  ],
+  declarations: [UserListPage, UserDetailPage, FabButtonComponent],
 })
 export class UsersModule {}
