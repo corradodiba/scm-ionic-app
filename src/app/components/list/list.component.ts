ba66ef7732ab38ac9c4b803ae025f869e61091e3
Corrado Dibattista <corradodiba@gmail.com>
2020-02-15 01:05:44 +0100

    hotfix/refactoring/lists

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,17 +1,33 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import Course from 'src/app/models/Course.model';
+import User from 'src/app/models/User.model';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
 })
-export class ListComponent implements OnInit {
-  @Input() disabled;
-  @Input() doInfinite() {};
-  numTimesLeft = 1;
+export class ListComponent {
+  @Input() items: Course[] | User[] = [];
+  @Input() isInfiniteScrollable: boolean;
+  itemsListView = [];
 
   constructor() {}
 
-  ngOnInit() {}
-
+  async doInfinite(event): Promise<Course | User> {
+    try {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          for (const item of this.items) {
+            this.itemsListView.push(item);
+          }
+          event.target.complete();
+          resolve();
+        }, 1000);
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
 }
